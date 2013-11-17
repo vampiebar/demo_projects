eb8c213cdfa7f48064cddc1a47b8762c06a71a05
cagla <cagla@cagla-pc>
2013-11-17 19:24:10 +0200

    xml egitim turu

---
+++ src/com/icarusdb/portal/icacourses/main/client/XMLEgitimTuru.java
@@ -0,0 +1,21 @@
+package com.icarusdb.portal.icacourses.main.client;
+
+import name.pehl.piriti.commons.client.Path;
+import name.pehl.piriti.xml.client.XmlReader;
+
+import com.google.gwt.core.client.GWT;
+
+public class XMLEgitimTuru {
+
+	interface VubReader extends XmlReader<XMLEgitimTuru> {
+	}
+
+	public static final VubReader XML = GWT.create(VubReader.class);
+
+	@Path("id")
+	String id;
+
+	@Path("egitim_turu_adi")
+	String egitim_turu_adi;
+
+}
