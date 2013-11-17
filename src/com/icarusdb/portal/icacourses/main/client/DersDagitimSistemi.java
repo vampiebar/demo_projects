3ab262e1f1aac92855337685d74460e355cee70e
vampie <vampie@192.168.2.4>
2013-11-17 19:23:12 +0200

    DersDagitim icin calender denemesi yapildi basarisiz. Component i kendim yazmak zorundayim

--- src/com/icarusdb/portal/icacourses/main/client/DersDagitimSistemi.java
+++ src/com/icarusdb/portal/icacourses/main/client/DersDagitimSistemi.java
@@ -2,6 +2,7 @@ package com.icarusdb.portal.icacourses.main.client;
 
 import java.util.Date;
 
+import com.bradrydzewski.gwt.calendar.client.Calendar;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.TextColumn;
@@ -11,15 +12,23 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.datepicker.client.DateBox;
 import com.google.gwt.user.datepicker.client.DateBox.DefaultFormat;
+import com.google.gwt.user.client.ui.SimplePanel;
 
 public class DersDagitimSistemi extends Composite {
+	private SimplePanel smpanCalendar;
 
 	public DersDagitimSistemi() {
+		
+//		Calendar calendar = new Calendar();
+//		calendar.setDate(new Date()); //calendar date, not required
+//		calendar.setDays(3); //number of days displayed at a time, not required
+//		calendar.setWidth("500px");
+//		calendar.setHeight("400px");
 
 		AbsolutePanel absolutePanel = new AbsolutePanel();
 		absolutePanel.setStyleName("gwt-dlgbackgorund");
 		initWidget(absolutePanel);
-		absolutePanel.setSize("714px", "410px");
+		absolutePanel.setSize("820px", "601px");
 
 		AbsolutePanel absolutePanel_1 = new AbsolutePanel();
 		absolutePanel_1.setStyleName("gwtabsolute");
@@ -40,10 +49,10 @@ public class DersDagitimSistemi extends Composite {
 		absolutePanel.add(btnRaporAl, 606, 49);
 		btnRaporAl.setSize("78px", "48px");
 
-		Label lblTarihSeiniz = new Label("Tarih Seçiniz");
+		Label lblTarihSeiniz = new Label("Tarih Seçiniz...");
 		lblTarihSeiniz.setStyleName("gwt-Bold");
-		absolutePanel.add(lblTarihSeiniz, 39, 121);
-		lblTarihSeiniz.setSize("75px", "26px");
+		absolutePanel.add(lblTarihSeiniz, 10, 121);
+		lblTarihSeiniz.setSize("104px", "26px");
 
 		DateBox dptTarihSeciniz = new DateBox();
 		dptTarihSeciniz.setStyleName("gwt-TextBox1");
@@ -52,17 +61,23 @@ public class DersDagitimSistemi extends Composite {
 		dptTarihSeciniz.setValue(new Date(1378037602295L));
 		absolutePanel.add(dptTarihSeciniz, 120, 121);
 		dptTarihSeciniz.setSize("143px", "17px");
-
-		CellTable<Object> grdDersDagitimSistemi = new CellTable<Object>();
-		absolutePanel.add(grdDersDagitimSistemi, 39, 165);
-		grdDersDagitimSistemi.setSize("611px", "198px");
-
-		TextColumn<Object> textColumn = new TextColumn<Object>() {
-			@Override
-			public String getValue(Object object) {
-				return object.toString();
-			}
-		};
-		grdDersDagitimSistemi.addColumn(textColumn, "02.09.2013 - 02.09.2013");
+		
+		smpanCalendar = new SimplePanel();
+		absolutePanel.add(smpanCalendar, 10, 153);
+		smpanCalendar.setSize("800px", "347px");
+		
+		if( !isDesignTime() ) {
+			
+			//smpanCalendar.add(calendar);
+		}
+		
 	}
+	
+	// Implement the following method exactly as-is
+	private static final boolean isDesignTime() {
+		return false; // GWT 2.2 and earlier
+		// return Beans.isDesignTime(); // GWT 2.4 and above
+
+	}
+
 }
